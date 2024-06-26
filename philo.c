b57766286809bce0da8cbb34a3a6bc79dc6f96b0
Elias Salmela <esalmela@c1r6p15.hive.fi>
2024-06-26 18:37:39 +0300

    inniting data

--- philo.c
+++ philo.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: esalmela <esalmela@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/26 12:00:23 by esalmela          #+#    #+#             */
+/*   Updated: 2024/06/26 12:00:25 by esalmela         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "philo.h"
 #include <stdio.h>
 
@@ -42,13 +54,21 @@ t_bool	ft_isdigit(char	*argument)
 	return (TRUE);
 }
 
-void	assign_data(t_philo *data, char **args)
+void	assign_data(t_overseer *data, char **args)
 {
+	int i;
+
+	i = 0;
+	data->number_of_philos = ft_atoi(args[1]);
 	data->time_to_die = ft_atoi(args[2]);
 	data->time_to_eat = ft_atoi(args[3]);
 	data->time_to_sleep = ft_atoi(args[4]);
 	if (args[5])
 		data->meals =  ft_atoi(args[5]);
+	data->philo = malloc(ft_atoi(args[1]) * sizeof(t_philo));
+	if (!data->philo)
+		printf("malloc error occured\n");
+
 }
 
 int check_arguments(int argc, char **argv)
@@ -58,23 +78,60 @@ int check_arguments(int argc, char **argv)
 	i = 0;
 	if (argc < 6 || argc > 7)
 	{
-		write(1, "Invalid number of arguments\n", 28);
+		write(2, "Invalid number of arguments\n", 28);
 		return (1);
 	}
 	while (++i < argc)
+	{
 		if (ft_isdigit(argv[i]) == FALSE)
-	{	
-		write(1, "Argument must be numeric type\n", 30);
+		{	
+			write(2, "Argument must be numeric type\n", 30);
 			return (1);
+		}
+		if (i == 1)
+		{
+			if (ft_atoi(argv[i]) < 1)
+				write(2, "Must be at least one philosopher\n", 34);
+		}
 	}
 	return (0);
 }
 
+void	*get_time()
+{
+	struct timeval tv;
+	long long milliseconds;
+	gettimeofday(&tv, NULL);
+	milliseconds = tv.tv_sec * 1000LL + tv.tv_usec / 1000; 
+    printf("Milliseconds: %lld\n", milliseconds);
+}
+
+void	set_utensil(t_overseer *overseer)
+{
+	pthread_mutex_t *forks;
+	int				i;
+
+	i = 0;
+	forks = malloc(overseer->number_of_philos * sizeof(pthread_mutex_t));
+		if (!forks)
+			printf("error\n");
+	
+}
+
+void	start_feast(t_overseer *overseer)
+{
+
+}
+
 int	main(int argc, char **argv)
 {
-	t_philo	data;
+	t_overseer	overseer;
 
 	if (check_arguments(argc, argv) == 1)
 		return (1);
-	assign_data(&data, argv);
+	assign_data(&overseer, argv);
+	set_utensils(&overseer);
+	star_feast(&overseer);
+	get_time();
+	free(overseer.philo);
 }
--- philo.h
+++ philo.h
@@ -1,8 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: esalmela <esalmela@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/26 12:00:58 by esalmela          #+#    #+#             */
+/*   Updated: 2024/06/26 12:00:59 by esalmela         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef PHILO_H
 # define PHILO_H
 
 # include <stdio.h>
 # include <unistd.h>
+# include <sys/time.h>
+# include <pthread.h>
+# include <stdlib.h>
 
 # define TRUE 1
 # define FALSE 0
@@ -11,9 +26,19 @@ typedef int	t_bool;
 
 typedef struct s_philo
 {
-	int time_to_die;
-	int time_to_eat;
-	int time_to_sleep;
-	int meals;	
+	pthread_t		philo;
+	pthread_mutex_t	left_fork;
+	pthread_mutex_t right_fork;
+	struct s_philo  *next;
 }	t_philo;
+
+typedef struct s_overseer
+{
+	int		time_to_die;
+	int		time_to_eat;
+	int		time_to_sleep;
+	int		number_of_philos;
+	int		meals;
+	t_philo	*philo;	
+}	t_overseer;
 #endif
